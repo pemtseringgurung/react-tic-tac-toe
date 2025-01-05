d0a50b8098bcb261f91ae87c3fa0a311c0b41e24
Pem Tsering Gurung <pemgurung541@gmail.com>
2025-01-05 11:50:16 -0500

    Added the feature if the user clicks on any of the square an X pops up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) =>  curActivePlayer === "X" ? "O" : "X");
+  }
   return (
     <main>
       <div id="game-container">
@@ -8,7 +15,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />     
         </ol>
-        Game Board
+        <GameBoard />
       </div>
       LOG
     </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,33 +1,38 @@
 import { useState } from "react";
 
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
 
-export default function Player({initialName, symbol}){
-    const [ playerName, setPlayerName ] = useState(initialName) 
-    const[ isEditing, setIsEditing] = useState(false);
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
 
-    function handleEditClick(){
-        setIsEditing(editing => !editing);
-    }
-
-    function handleChange(event){
-        setPlayerName(event.target.value);
-    }
-    let editablePlayerName =   <span className='player-name'>{playerName}</span>;
-    let btnCaption = 'Edit'
-
-    if (isEditing){
-        editablePlayerName = <input className = "player-input" type="text" required value = {playerName} onChange={handleChange} />;
-        btnCaption = "Save"
-
-    }
-    return (
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        className="player-input"
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    );
+    btnCaption = "Save";
+  }
+  return (
     <li>
-        <span id="player">
-            {editablePlayerName}
-            <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleEditClick}>{btnCaption}</button>
+      <span id="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
-    )
+  );
 }
